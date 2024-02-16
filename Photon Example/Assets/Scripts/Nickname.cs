30dabd0b4b40285c5a1bc69b45158dc3a9d67cdb
7erz <91477084+7erz@users.noreply.github.com>
2024-02-16 19:45:41 +0900

    240216 네트워크 객체 생성

---
+++ Photon Example/Assets/Scripts/Nickname.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class Nickname : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TextMeshProUGUI nickName;
+
+    private void Start()
+    {
+        nickName.text = photonView.Owner.NickName;
+    }
+
+
+}
